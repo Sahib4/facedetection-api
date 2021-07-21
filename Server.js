0da97929761729151cccd782dcf30a6b4d5954de
Sahib4 <68962355+Sahib4@users.noreply.github.com>
2021-07-21 14:26:20 -0500

    facedetection-api

---
+++ Server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+const knex = require('knex')
+const { response } = require('express')
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'sahibnarang',
+    password: '',
+    database: 'facedetection-database',
+  },
+})
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send(database.users)
+})
+
+app.post('/signin', (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt)
+})
+
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt)
+})
+
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db)
+})
+
+app.put('/image', (req, res) => {
+  image.imageHandler(req, res, db)
+})
+
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`app is running on port ${process.env.PORT}`)
+})
---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const { json } = require('body-parser')
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+  apiKey: 'e5d67599ed3a4d78b1851de7af9ef366',
+})
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((err) => res.status(400).json('unble to work with API'))
+}
+
+const imageHandler = (req, res, db) => {
+  const { id } = req.body
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      res.json(entries)
+    })
+    .catch((err) => res.status(400).json('unable to get count'))
+}
+
+module.exports = {
+  imageHandler: imageHandler,
+  handleApiCall: handleApiCall,
+}
